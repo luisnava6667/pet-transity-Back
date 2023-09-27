31b9e070710782f0d8f11b79f5f14cf676253376
luisnava6667 <navaluisrodolfo@gmail.com>
2023-09-27 08:28:24 -0300

    update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 
+const app = express()
 
 dotenv.config()
-
 app.use(express.json())
 
 connectDB()
