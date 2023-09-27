3dbb94cb71f340da72268f6be3f064e6a4739fc2
luisnava6667 <navaluisrodolfo@gmail.com>
2023-09-27 08:18:01 -0300

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 
 app.use(express.json())
 
-const connectDB = require('./connectMongo')
+const connectDB = require('./config/db')
 
 connectDB()
 
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,1 @@
-const us
+// const us
