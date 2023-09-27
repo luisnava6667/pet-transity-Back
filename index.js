bf187572faa0cdcf0eb35b7e31118f0acb4b9c5b
luisnava6667 <navaluisrodolfo@gmail.com>
2023-09-27 08:06:53 -0300

    update

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const connectDB = require('./config/db.js')
-const usuarioRoutes = require('./routes/usuarioRoutes.js')
 
 const app = express()
+
+require('dotenv').config()
+
 app.use(express.json())
-dotenv.config()
+
+const connectDB = require('./connectMongo')
+
 connectDB()
 
-app.use((_req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  next()
+// app.use((_req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//   next()
+// })
+app.get('/', async (req, res) => {
+  res.send('Hello World!')
 })
-app.use('/', usuarioRoutes)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`⚡️ Server running on port ${PORT}!!! ⚡️`)
