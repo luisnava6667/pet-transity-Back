6457055e2dd595e150fd7dc7aaf5f395fd873dba
luisnava6667 <navaluisrodolfo@gmail.com>
2023-10-04 00:06:56 -0300

    update

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ connectDB()
 //   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 //   next()
 // })
-
+app.use((_req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 app.use('/usuarios', usuarioRoutes)
 app.use('/refugio', refugioRoutes)
 app.use('/animales', animalesRoutes)
